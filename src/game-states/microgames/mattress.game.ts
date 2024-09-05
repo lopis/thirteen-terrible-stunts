3aeff9deb0deaa343f585fa00460b2dfeb36f8ba
Joao Lopes <lopis@users.noreply.github.com>
2024-09-05 16:00:39 +0200

    mattress difficulty

--- src/game-states/microgames/mattress.game.ts
+++ src/game-states/microgames/mattress.game.ts
@@ -3,11 +3,26 @@ import { BuildingJumpGame } from "./building-jump.game";
 import { Building } from "@/core/entities/building";
 import { colors, HEIGHT, WIDTH } from "@/core/draw-engine";
 import { Platform } from "@/core/entities/platform";
+import { gameData } from "@/core/game-data";
+import { interpolate } from "@/util/util";
+
+// Difficulty range of each property
+const difficultyRange: Record<string, [number,number]> = {
+  goalSize: [4, 2],
+  buildingHeigh: [100, 50],
+};
 
 class MattressGame extends BuildingJumpGame {
+  goalSize = 0;
+  buildingHeigh = 0;
+
   onEnter() {
     super.onEnter();
     this.text = 'Land safely';
+
+    const difficulty = gameData.getDifficulty();
+    this.goalSize = interpolate(difficultyRange.goalSize, difficulty);
+    this.buildingHeigh = interpolate(difficultyRange.buildingHeigh, difficulty);
 
     this.deathColliders = [new Platform(
       { x: 0, y: HEIGHT - 20 },
@@ -16,13 +31,13 @@ class MattressGame extends BuildingJumpGame {
     )];
     const goal = new Platform(
       {x: WIDTH - 102, y: HEIGHT - 25},
-      {x: CHARACTER_SIZE * 2, y: 5},
+      {x: CHARACTER_SIZE * this.goalSize, y: 5},
       colors.black,
       colors.white,
     );
     this.goalColliders = [goal];
     this.platforms = [
-      new Building({x: -20, y: 50 + CHARACTER_SIZE}, 4),
+      new Building({x: -20, y: this.buildingHeigh + CHARACTER_SIZE}, 4),
       goal,
     ];
   }
